58d49932695ffdbf621813310dad4b945e9f4071
Victor <victarv@gmail.com>
2020-04-23 11:44:32 +0200

    Rename token.py -> token_simsims.py

--- OU3/token_simsims.py
+++ OU3/token_simsims.py
@@ -1,4 +1,5 @@
 import json
+from simulation import Simulation
 
 
 class Token():
@@ -12,39 +13,39 @@ class Token():
         '''Returns tokens gui component.'''
         return self._gui_component
 
-    def remove_gui_component(self, gui):
+    def remove_gui_component(self):
         '''Removes own gui component from gui.'''
         if self._gui_component:
-            gui.remove(self._gui_component)
+            Simulation.gui.remove(self._gui_component)
 
-    def create_gui_component(self, gui):
+    def create_gui_component(self):
         raise NotImplementedError
 
 
 class Product(Token):
     '''Product type token. Subclass to Token.'''
 
-    def __init__(self, gui):
+    def __init__(self):
         super().__init__()
-        self.create_gui_component(gui)
+        self.create_gui_component()
 
-    def create_gui_component(self, gui):
+    def create_gui_component(self):
         '''Creates a red token gui component and adds it to the gui.'''
         properties = {'color': '#ff0000'}
-        self._gui_component = gui.create_token_ui(properties)
+        self._gui_component = Simulation.gui.create_token_ui(properties)
 
 
 class Food(Token):
     '''Food type token. Subclass to Token.'''
 
-    def __init__(self, gui):
+    def __init__(self):
         super().__init__()
-        self.create_gui_component(gui)
+        self.create_gui_component()
 
-    def create_gui_component(self, gui):
+    def create_gui_component(self):
         '''Creates a green token gui component and adds it to the gui.'''
         properties = {'color': '#00ff00'}
-        self._gui_component = gui.create_token_ui(properties)
+        self._gui_component = Simulation.gui.create_token_ui(properties)
 
 
 class Worker(Token):
@@ -52,9 +53,9 @@ class Worker(Token):
 
     max_health = 100
 
-    def __init__(self, gui):
+    def __init__(self):
         super().__init__()
-        self.create_gui_component(gui)
+        self.create_gui_component()
         self._health = Worker.max_health
 
     @property
@@ -62,7 +63,7 @@ class Worker(Token):
         '''Returns worker's health.'''
         return self._health
 
-    @health.setter
+    @get_health.setter
     def health(self, health):
         '''Sets workers health if health is in a valid range, otherwise raises ValueError.'''
         if 0 < health <= Worker.max_health:
@@ -70,10 +71,10 @@ class Worker(Token):
         else:
             raise ValueError
 
-    def create_gui_component(self, gui):
+    def create_gui_component(self):
         '''Creates a red token gui component and adds it to the gui.'''
-        properties = {'color': '000000'}
-        self._gui_component = gui.create_token_ui(properties)
+        properties = {'color': '#000000'}
+        self._gui_component = Simulation.gui.create_token_ui(properties)
 
     def decrease_health(self, amount):
         '''Removes health from worker, does not check if worker's health is reduced to or below 0.'''
@@ -84,13 +85,13 @@ class Worker(Token):
         self._health += amount
         self._health = min(self._health, Worker.max_health)
 
-    def to_json(self):
+    def to_dict(self):
         '''Serializes worker to a JSON-string.'''
-        return json.dumps({'health': self._health})
+        return {'health': self._health}
 
     @classmethod
-    def from_json(cls, data, gui):
+    def from_dict(cls, data):
         '''Creates and returns a worker from a json object.'''
-        worker = cls(gui)
+        worker = cls()
         worker.health = data['health']
         return worker
