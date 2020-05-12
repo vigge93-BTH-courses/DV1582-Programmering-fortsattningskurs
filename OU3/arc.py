310a329d9a0f1512775fe33e090e4bcd4fac56ff
Victor <victarv@gmail.com>
2020-05-12 23:13:08 +0200

    removed locks from arc

--- OU3/arc.py
+++ OU3/arc.py
@@ -1,6 +1,5 @@
 """Module for arc that handles transportation of tokens."""
 from time import sleep
-from threading import Lock
 
 
 class Arc():
@@ -11,58 +10,42 @@ class Arc():
     def __init__(self, sim):
         """Create an arc object."""
         self._sim = sim
-        self._lock = Lock()
 
     def get_worker(self):
         """Get a worker from the road. If the road is empty, return None."""
         sleep(Arc.transport_time)
-        self._lock.acquire()
         try:
             return self._sim.get_road.remove()
         except RuntimeError:
             return None
-        finally:
-            self._lock.release()
 
     def get_food(self):
         """Get a food from the shed. If the shed is empty, return None."""
         sleep(Arc.transport_time)
-        self._lock.acquire()
         try:
             return self._sim.get_shed.remove()
         except RuntimeError:
             return None
-        finally:
-            self._lock.release()
 
     def get_product(self):
         """Get product from the magazine. If magazine is empty, return None."""
         sleep(Arc.transport_time)
-        self._lock.acquire()
         try:
             return self._sim.get_magazine.remove()
         except RuntimeError:
             return None
-        finally:
-            self._lock.release()
 
     def store_worker(self, worker):
         """Store a worker on the road."""
         sleep(Arc.transport_time)
-        self._lock.acquire()
         self._sim.get_road.add(worker)
-        self._lock.release()
 
     def store_food(self, food):
         """Store a food in the shed."""
         sleep(Arc.transport_time)
-        self._lock.acquire()
         self._sim.get_shed.add(food)
-        self._lock.release()
 
     def store_product(self, product):
         """Store a product in the magazine."""
         sleep(Arc.transport_time)
-        self._lock.acquire()
         self._sim.get_magazine.add(product)
-        self._lock.release()
